ff0379904c22bf5096fca93ff1404d8bff494c0e
Mohammad Tahir Khan <tahir026khan@gmail.com>
2023-12-11 20:55:55 +0000

    nice

--- text assignment 2/save_output.py
+++ text assignment 2/save_output.py
@@ -1,12 +1,13 @@
+import pandas as pd
+
 class SaveOutput:
     def __init__(self, number_classes, user_id):
         self.number_classes = number_classes
         self.user_id = user_id
 
     def save_output(self, sentence_ids: list, predicted_labels: list, dataset_name: str) -> None:
-        lines_to_write = ["SentenceID\tSentiment\n"]
-        lines_to_write.extend([f"{sentence_ids[i]}\t{predicted_labels[i]}\n" for i in range(len(sentence_ids))])
+        data = {"SentenceID": sentence_ids, "Sentiment": predicted_labels}
+        df = pd.DataFrame(data)
 
         output_filename = f'{dataset_name}_predictions_{self.number_classes}classes_{self.user_id}.tsv'
-        with open(output_filename, 'w') as f:
-            f.writelines(lines_to_write)
+        df.to_csv(output_filename, sep='\t', index=False)
